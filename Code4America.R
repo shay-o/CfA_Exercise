4454951e4a130b7048dc7fb1e2173970a0b58e37
ShayO <shay_o_reilly@hotmail.com>
2017-11-12 20:36:39 -0800

    Setup
    Initial Github setup

---
+++ Code4America.R
@@ -0,0 +1,17 @@
+# Plan
+# .rmd for sharing
+# Box plot to explore
+# ? Simple regression?
+# What is goal? More people on food stamps. 
+  # This could be more applications from likely converters. Could be outreach
+      #...where to find appliers
+  # ...or more conversions by changing some attribute that is changeable
+
+#Effort could be applied where there are rejections. 
+
+# Theories
+  # Phone interviews will be less likely to be approved
+  # Reminders are helpful
+  # Will be significant skew in gender and age. 
+  # Perhaps a low volume high-converting category will be one to targetn.
+
